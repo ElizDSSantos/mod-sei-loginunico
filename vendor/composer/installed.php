d8f6ffe437c04b99198a3553314df4f3c07babff
fe-neto <54376460+fe-neto@users.noreply.github.com>
2021-02-11 12:55:55 -0300

    feat(instalação): Adiciona a pasta vendor
    Para não obrigar o órgão a instalar o composer, adicionamos a pasta vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cf46552249b587c884bf21356b4208d0e6710832',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cf46552249b587c884bf21356b4208d0e6710832',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+  ),
+);
